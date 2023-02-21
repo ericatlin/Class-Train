169af3cdf5a6567021db0038f81583cf0bac1424
林智珉 <eriksonop@gmail.com>
2023-02-21 13:29:50 +0800

    ?

--- Class-Train-2-test/Program.cs
+++ Class-Train-2-test/Program.cs
@@ -12,8 +12,8 @@ namespace Class_Train_2_test
         {
             Console.WriteLine("請輸入以逗號分隔的整數");
             string[] allNum = Console.ReadLine().Split(',');
-            Console.WriteLine($"奇數:{string.Join(",", allNum.Where(x => int.Parse(x) % 2 == 0))}");
-            Console.WriteLine($"偶數:{string.Join(",", allNum.Where(x => int.Parse(x) % 2 != 0))}");
+            Console.WriteLine($"奇數:{string.Join(",", allNum.Where(x => int.Parse(x) % 2 != 0))}");
+            Console.WriteLine($"偶數:{string.Join(",", allNum.Where(x => int.Parse(x) % 2 == 0))}");
             Console.ReadLine();
         }
     }
